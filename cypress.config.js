1dba80c9212a07f0136a5afbddce4e2a5e659c9e
Gabriel Potamianos <gpotamianos@theworkshop.com>
2025-06-07 23:14:14 +0200

    CI Configs Initial Commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://localhost:1234',
     specPattern: 'cypress/e2e/**/*.spec.js',
     supportFile: false,
     experimentalFetchPolyfill: true,  // so fetch intercepts work
